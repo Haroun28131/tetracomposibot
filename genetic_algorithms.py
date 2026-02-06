0ac8af61e93dc0bb1450c6a7811d01daea86827e
Haroun28131 <haroun.ellias.zerdoumi@gmail.com>
2026-02-06 10:56:12 +0100

    Update genetic_algorithms.py

--- genetic_algorithms.py
+++ genetic_algorithms.py
@@ -32,10 +32,10 @@ class Robot_player(Robot):
         self.evaluations = evaluations
         self.it_per_evaluation = it_per_evaluation
         # new variables
-        self.param = np.random.randint( -1, 2, size=(18, ) )
-        self.score = np.empty( shape=(self.it_per_evaluation, ) )
+        self.param = np.random.randint( -1, 2, size=(8, ) )
+        self.ps = 0
         self.best_robot_config = None
-        self.prev_trans, self.prev_rotation = 0.0, self.theta_0
+        self.theta_0
         ###############
         super().__init__(x_0, y_0, theta_0, name=name, team=team)
 
@@ -54,21 +54,19 @@ class Robot_player(Robot):
         sensors_nda = np.array(sensors)
 
         # fonction de contrôle (qui dépend des entrées sensorielles, et des paramètres)
-        translation = np.tanh ( self.param[0] + self.param[1:9] @  sensors_nda )
-        rotation = np.tanh ( self.param[9] + self.param[10:] @  sensors_nda )
+        translation = math.tanh ( self.param[0] + self.param[1] * sensors[sensor_front_left] + self.param[2] * sensors[sensor_front] + self.param[3] * sensors[sensor_front_right] )
+        rotation = math.tanh ( self.param[4] + self.param[5] * sensors[sensor_front_left] + self.param[6] * sensors[sensor_front] + self.param[7] * sensors[sensor_front_right] )
         # self.score[ self.iteration % self.it_per_evaluation ] = translation * (1 - np.abs(rotation)) * np.min(sensors_nda) * np.max(sensors_nda)
         # print(np.abs(self.log_sum_of_rotation))
-        self.score[ self.iteration % self.it_per_evaluation ] = (self.log_sum_of_translation-self.prev_trans) * (1 - np.abs(self.log_sum_of_rotation - self.prev_rotation))
         
         # self.score[ self.iteration % self.it_per_evaluation ] = (self.log_sum_of_translation) * (1 - np.abs(self.log_sum_of_rotation - self.prev_rotation))
         self.iteration = self.iteration + 1 
-
-        self.prev_trans, self.prev_rotation = self.log_sum_of_translation, self.log_sum_of_rotation
-
+        current_score = self.log_sum_of_translation + (1 - np.abs(self.log_sum_of_rotation)*1.1) 
 
         # toutes les X itérations: le robot est remis à sa position initiale de l'arène avec une orientation aléatoire
         if self.iteration % self.it_per_evaluation == 0:
-            current_score = np.sum( self.score )
+            
+            #print(f"### Current param : {np.array2string(self.param, separator=', ')}, score = {current_score}")
             if self.best_robot_config is None:
                 self.best_robot_config = (current_score, self.param, self.trial)
 
@@ -80,18 +78,26 @@ class Robot_player(Robot):
                     self.best_robot_config = (current_score, self.param, self.trial)
             # creation du fils et mutation
             best_score, best_param, trial_found = self.best_robot_config
-            best_param = copy.deepcopy(best_param)
-            while(True):
+            mut_param = copy.deepcopy(best_param)
+            """while(True):
                 ind = np.random.randint(0, len(best_param))
                 muter = np.random.randint( -1, 2 )
                 if best_param[ind] != muter : 
                     best_param[ind] = muter
-                    break
+                    break"""
+            i=np.random.randint(0, len(best_param))
+            m=np.random.randint( -1, 2 )
+            while (mut_param[i]==m):
+                m=np.random.randint( -1, 2 )
+            mut_param[i]=m
+
             print(f"the best robot found : {np.array2string(best_param, separator=', ')} with score = {best_score} found in try number {trial_found}")
-            self.param = best_param
+            print(np.array2string(best_param, separator=', '))
+            print(np.array2string(mut_param, separator=', '), "score : ", current_score)
+            self.param = mut_param
             self.trial = self.trial + 1
             print ("Trying strategy no.",self.trial)
-            print(ind, muter)
+            print(i, m)
             return 0, 0, True # ask for reset     
 
         return translation, rotation, False
