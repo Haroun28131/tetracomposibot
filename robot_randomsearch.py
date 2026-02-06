b37b9267aee7f8a0798df67ea04bbc1688a8501c
Haroun28131 <haroun.ellias.zerdoumi@gmail.com>
2026-02-06 10:56:50 +0100

    Update robot_randomsearch.py

--- robot_randomsearch.py
+++ robot_randomsearch.py
@@ -31,8 +31,8 @@ class Robot_player(Robot):
         self.evaluations = evaluations
         self.it_per_evaluation = it_per_evaluation
         # new variables
-        self.param = np.random.randint( -1, 2, size=(18, ) )
-        self.score = np.empty( shape=(self.it_per_evaluation, ) )
+        self.param = np.random.randint( -1, 2, size=(8, ) )
+        self.score = 0
         self.best_robot_config = None
         ###############
         super().__init__(x_0, y_0, theta_0, name=name, team=team)
@@ -50,17 +50,21 @@ class Robot_player(Robot):
 
         
         sensors_nda = np.array(sensors)
+                    #print ("\tparameters           =",self.param)
+                    #print ("\ttranslations         =",self.log_sum_of_translation,"; rotations =",self.log_sum_of_rotation) # *effective* translation/rotation (ie. measured from displacement)
+                    #print ("\tdistance from origin =",math.sqrt((self.x-self.x_0)**2+(self.y-self.y_0)**2))
 
         # fonction de contrôle (qui dépend des entrées sensorielles, et des paramètres)
-        translation = np.tanh ( self.param[0] + self.param[1:9] @  sensors_nda )
-        rotation = np.tanh ( self.param[9] + self.param[10:] @  sensors_nda )
-        self.score[ self.iteration % self.it_per_evaluation ] = translation * (1 - np.abs(rotation)) * np.min(sensors_nda) * np.max(sensors_nda)
+        translation = math.tanh ( self.param[0] + self.param[1] * sensors[sensor_front_left] + self.param[2] * sensors[sensor_front] + self.param[3] * sensors[sensor_front_right] )
+        rotation = math.tanh ( self.param[4] + self.param[5] * sensors[sensor_front_left] + self.param[6] * sensors[sensor_front] + self.param[7] * sensors[sensor_front_right] )
+        current_score = self.log_sum_of_translation + (1 - np.abs(self.log_sum_of_rotation)) 
+      
 
         self.iteration = self.iteration + 1  
 
         # toutes les X itérations: le robot est remis à sa position initiale de l'arène avec une orientation aléatoire
         if self.iteration % self.it_per_evaluation == 0:
-            current_score = np.sum( self.score )
+            
             if self.best_robot_config is None:
                 self.best_robot_config = (current_score, self.param, self.trial)
             else:
@@ -69,9 +73,10 @@ class Robot_player(Robot):
                     self.best_robot_config = (current_score, self.param, self.trial)
             best_score, best_param, trial_found = self.best_robot_config
             print(f"the best robot found : {np.array2string(best_param, separator=', ')} with score = {best_score} found in try number {trial_found}")
-            self.param = np.random.randint( -1, 2, size=(18, ) )
+            self.param = np.random.randint( -1, 2, size=(8, ) )
             self.trial = self.trial + 1
             print ("Trying strategy no.",self.trial)
             return 0, 0, True # ask for reset     
 
         return translation, rotation, False
+
